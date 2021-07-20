8f58dcb241ab353c6d86666ac954bedd025ae9f5
gm2202988 <kmk@gabia.com>
2021-07-20 19:36:28 +0900

    feat: swagger 인증 추가

--- src/main/java/com/gabia/logservice/config/SwaggerConfig.java
+++ src/main/java/com/gabia/logservice/config/SwaggerConfig.java
@@ -5,11 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -20,21 +23,41 @@ public class SwaggerConfig {
 
     @Bean
     public Docket apiV1() {
-
         return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
+                .apiInfo(apiInfo(TITLE, VERSION))
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.gabia"))
                 .paths(PathSelectors.any())
                 .build();
     }
 
-    private ApiInfo apiInfo() {
+    private ApiInfo apiInfo(String title, String version) {
         return new ApiInfoBuilder()
-                .title(TITLE)
-                .description("Swagger로 생성한 Log Service API 문서")
-                .version(VERSION)
-                .contact(new Contact("Gabia", "www.gabia.com", "gabia@gabia.com"))
-                .build();
+                .title(title)
+                .description(String.format("%s 문서", title))
+                .version(version)
+                .contact(new Contact(
+                        "Gabia",
+                        "www.gabia.com",
+                        "gabia@gabia.com"
+                )).build();
     }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+    }
+
 }
